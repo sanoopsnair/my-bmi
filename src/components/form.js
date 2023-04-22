3d2c20c56942d25db88e343965a578bf497eb48f
sanoop <sanoop.snair@gmail.com>
2023-04-23 01:00:33 +0400

    Completed

--- src/components/form.js
+++ src/components/form.js
@@ -1,31 +1,61 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
-function form() {
+function Form({getData}) {
   const [weight, setWeight] = useState("");
   const [height, setHeight] = useState("");
+  const [alert, setAlert] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (isNaN(weight) || isNaN(height)) {
+      console.log("Not a valid input");
+      setAlert(true);
+    } else {
+      setAlert(false);
+      getData(weight, height);
+      setHeight("");
+      setWeight("");
+    }
+  }
 
   return (
     <div className="col-sm-4 shadow rounded px-5">
       <h1 className="text-center pt-3 text-secondary h2">BMI Calculator</h1>
-      <form >
+      <form onSubmit={onSubmit}>
         <div className="row">
           <div className="col col-sm-6">
             <div className="my-3">
               <label className="form-label">Weight(kg) :</label>
-              <input type="text" className="form-control" />
+              <input type="text" 
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+              className="form-control" 
+              required
+              />
             </div>
           </div>
           <div className="col col-sm-6">
             <div className="my-3">
               <label className="form-label">Hight(m) :</label>
-              <input type="text" className="form-control" />
+              <input type="text" 
+              value={height}
+              onChange={(e) => setHeight(e.target.value)}
+              required
+              className="form-control"
+               />
             </div>
           </div>
         </div>
         <input type="submit" className="btn btn-primary my-3" value="Get BMI" />
       </form>
+      {alert && (
+        <div className="alert alert-danger" role="alert">
+          Plz enter valid datas
+        </div>
+      )}
     </div>
   );
 }
 
-export default form
+export default Form
